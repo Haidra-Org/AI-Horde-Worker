b7d07aae6fcbd4f60b5c43de57bfd9efe21da0d1
Henk <henk@henk.tech>
2022-10-17 17:58:06 +0200

    Typo Fix

--- update-runtime.sh
+++ update-runtime.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -qO- https://micromamba.snakepit.net/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
-bin\micromamba create --no-shortcuts -r conda -n linux -f environment.yaml -y
+bin/micromamba create --no-shortcuts -r conda -n linux -f environment.yaml -y
