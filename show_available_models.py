2635b9aaabfa35b2db56268601bbae8d9ceedb4b
Divided by Zer0 <mail@dbzer0.com>
2022-10-19 11:34:08 +0200

    model metadata

---
+++ show_available_models.py
@@ -0,0 +1,14 @@
+# test_download_models
+from nataili.model_manager import ModelManager
+
+# TODO: huggingface_hub or some way to use token instead of username/password
+mm = ModelManager()
+
+filtered_models = mm.get_filtered_models(type='ckpt')
+ppmodels = ''
+for model_name in filtered_models:
+    ppmodels += model_name
+    if filtered_models[model_name].get('description'):
+        ppmodels += f" : {filtered_models[model_name].get('description')}"
+    ppmodels += '\n'
+print(f"## Known ckpt Models ##\n{ppmodels}")
