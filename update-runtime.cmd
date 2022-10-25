f02562e11e466c56063f62f175113ee416a6edb7
henk717 <henk@henk.tech>
2022-10-25 18:13:52 +0200

    Fix for conda conflicts (#13)

--- update-runtime.cmd
+++ update-runtime.cmd
@@ -1,5 +1,6 @@
 @echo off
 cd /d %~dp0
+SET CONDA_SHLVL=
 
 Reg add "HKLM\SYSTEM\CurrentControlSet\Control\FileSystem" /v "LongPathsEnabled" /t REG_DWORD /d "1" /f 2>nul
 umamba create --no-shortcuts -r conda -n windows -f environment.yaml -y
