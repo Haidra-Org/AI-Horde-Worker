ee76bf548da4ac8af1f45dace152d5535e0111a8
Donald Nixon <donald@nodero.com>
2023-01-30 16:14:02 +1300

    double the denoise strength

--- nataili/inference/compvis_pix2pix.py
+++ nataili/inference/compvis_pix2pix.py
@@ -116,7 +116,7 @@ class CompVisPix2Pix:
             init_img = resize_image(resize_mode, init_img, width, height)
         
         assert 0.0 <= denoising_strength <= 1.0, "can only work with strength in [0.0, 1.0]"
-        t_enc = int(denoising_strength * ddim_steps)
+        denoising_strength = int(denoising_strength  * 2)
 
         def sample_pix2pix(
             init_data,
