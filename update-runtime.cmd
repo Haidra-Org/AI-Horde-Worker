75aea1042fdfea7213004a8bdc0c2bbc3a745587
henk717 <henk@henk.tech>
2023-03-01 15:24:10 +0100

    Conda workaround for Windows (Same as Linux) (#64)

--- update-runtime.cmd
+++ update-runtime.cmd
@@ -4,7 +4,10 @@ cd /d "%~dp0"
 SET CONDA_SHLVL=
 
 Reg add "HKLM\SYSTEM\CurrentControlSet\Control\FileSystem" /v "LongPathsEnabled" /t REG_DWORD /d "1" /f 2>nul
+:We do this twice the first time to workaround a conda bug where pip is not installed correctly the first time - Henk
+IF EXIST CONDA GOTO WORKAROUND_END
 umamba create --no-shortcuts -r conda -n windows -f environment.yaml -y
-call conda\condabin\activate.bat windows
-pip install -r requirements.txt
+:WORKAROUND_END
+umamba create --no-shortcuts -r conda -n windows -f environment.yaml -y
+umamba run -r conda -n windows pip install -r requirements.txt
 echo If there are no errors above everything should be correctly installed (If not, try running update_runtime.cmd as admin).
