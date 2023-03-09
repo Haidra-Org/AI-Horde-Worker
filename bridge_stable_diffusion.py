f4b0b524ac91e4728701b5e2514ca68a6533c93b
ResidentChief <35794623+ResidentChief@users.noreply.github.com>
2023-03-10 12:06:56 +1300

    Adds CSAM post-processing filter to AI Horde Worker
    Will scan all generated images for CSAM and censor any that appear. 
    This will consume a bit more VRAM and a second more per request.

--- bridge_stable_diffusion.py
+++ bridge_stable_diffusion.py
@@ -45,6 +45,7 @@ def main():
 
     bridge_data = StableDiffusionBridgeData()
     model_manager = ModelManager(
+        clip=True,
         compvis=True,
         diffusers=True,
         esrgan=True,
