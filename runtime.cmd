be864bcb52ed7488dad3728b78bdc6092652c92f
Henk <henk@henk.tech>
2023-03-13 17:48:17 +0100

    Strict Isolation

--- runtime.cmd
+++ runtime.cmd
@@ -1,7 +1,11 @@
 @echo off
 cd /d "%~dp0"
 
+:Isolation
 SET CONDA_SHLVL=
+set path=%windir%\system32;%windir%;%windir%\System32\Wbem;%windir%\System32\WindowsPowerShell\v1.0\
+set appdata=%~dp0conda\windows-appdata
+set pythonpath=
 
 Reg add "HKLM\SYSTEM\CurrentControlSet\Control\FileSystem" /v "LongPathsEnabled" /t REG_DWORD /d "1" /f 2>nul
 IF EXIST CONDA GOTO APP
