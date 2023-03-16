331f38d5c4bbc8b4a51b15f82f351c8051d7e876
Donald Nixon <donald@nodero.com>
2023-03-16 16:13:24 +1300

    Add extra piece to submit dictionary

--- worker/jobs/stable_diffusion.py
+++ worker/jobs/stable_diffusion.py
@@ -397,6 +397,7 @@ class StableDiffusionHordeJob(HordeJobFramework):
         else:
             logger.info("Skipping to post-processors")
             self.image = self.pop.get("source_image")
+            self.seed = 1
 
         # Run Post-Processors
         for post_processor in self.current_payload.get("post_processing", []):
