503a7786545219d2afabfd460e0675937fb7f1cc
Jug <jugdev@proton.me>
2023-03-25 10:53:22 +0000

    GPU info pretty prints.

--- worker/gpu.py
+++ worker/gpu.py
@@ -1,5 +1,6 @@
 import contextlib
 import os
+import json
 
 from pynvml.smi import nvidia_smi
 
@@ -81,4 +82,4 @@ class GPUInfo:
 
 if __name__ == "__main__":
     gpu = GPUInfo()
-    print(gpu.get_info())
+    print(json.dumps(gpu.get_info(), indent=4))
