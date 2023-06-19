097d95e67949e1a39e8da27ae3c223079fced8ad
Rob Ballantyne <rob@dynamedia.uk>
2023-06-19 10:40:47 +0100

    newline

--- webui.py
+++ webui.py
@@ -919,4 +919,3 @@ if __name__ == "__main__":
         parser.error("--user and --password must both be specified")
     ui = WebUI()
     ui.run(args.share, args.no_browser, args.lan, args.user, args.password)
-    
