9f2fc77c3d398cb5809ef144e02608b308405c89
tazlin <tazlin.on.github@gmail.com>
2023-07-01 09:13:19 -0400

    fix: translates mamba install fix to linux

--- runtime.sh
+++ runtime.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# Get the directory of the current script
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+# Build the absolute path to the Conda environment
+CONDA_ENV_PATH="$SCRIPT_DIR/conda/envs/linux/lib"
+
+# Add the Conda environment to LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="$CONDA_ENV_PATH:$LD_LIBRARY_PATH"
+export MAMBA_ROOT_PREFIX="$SCRIPT_DIR/conda"
+
 if [ ! -f "conda/envs/linux/bin/python" ]; then
 ./update-runtime.sh
 fi
