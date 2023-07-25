5b2206854e4ab20e4aeecdc152b93ddd16c536c8
tazlin <tazlin.on.github@gmail.com>
2023-07-25 18:12:35 -0400

    feat: preload/build cache for models

--- worker/utils/set_envs.py
+++ worker/utils/set_envs.py
@@ -13,7 +13,16 @@ def set_worker_env_vars_from_config():
             config = yaml.safe_load(configfile)
             if "cache_home" in config:
                 os.environ["AIWORKER_CACHE_HOME"] = config["cache_home"]
-            if "max_lora_cache_size" in config:
-                os.environ["HORDE_MAX_LORA_CACHE"] = str(config["max_lora_cache_size"])
+            if "temp_dir" in config:
+                os.environ["AIWORKER_TEMP_DIR"] = config["temp_dir"]
         return True
     return False
+
+
+def get_models_to_load():
+    config_file_as_path = Path(BRIDGE_CONFIG_FILE)
+    if config_file_as_path.exists():
+        with open(config_file_as_path, "rt", encoding="utf-8", errors="ignore") as configfile:
+            config = yaml.safe_load(configfile)
+            return config.get("models_to_load", [])
+    return None
