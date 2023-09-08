9f3c533240ed17e2bab1c27ad6611b0851054835
tazlin <tazlin.on.github@gmail.com>
2023-09-08 07:26:13 -0400

    style: ruff catchup

--- show_available_models.py
+++ show_available_models.py
@@ -17,9 +17,9 @@ if __name__ == "__main__":
     mm = SharedModelManager.manager
 
     filtered_models = mm.compvis.get_filtered_models(type="ckpt")
-    
+
     sorted_model_names = sorted(filtered_models.keys())
-    
+
     ppmodels = ""
     for model_name in sorted_model_names:
         if model_name == "LDSR":
